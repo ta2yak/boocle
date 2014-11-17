97e85fb49794f4b8b6fbd2ed569c08489987e65e
kawasaki tatsuya <ta2ya.xxx@gmail.com>
2014-11-17 15:10:29 +0900

    書籍にコメント機能を追加

--- public/js/components/book_title.js
+++ public/js/components/book_title.js
@@ -1,8 +1,8 @@
 define(['react', "jquery", "underscore", 
-        "models/session", "models/book_title_list",  "models/book_owner_list", 
+        "models/session", "models/book_title_list",  "models/book_owner_list", "models/comment_list", 
         "mixins/authority"],
 function(React, $, _, 
-         SessionManager, BookTitleListModel, BookOwnerListModel, 
+         SessionManager, BookTitleListModel, BookOwnerListModel, CommentListModel, 
          AuthorityMixin){
 
   // ****************************************************
@@ -147,7 +147,7 @@ function(React, $, _,
         );
       }.bind(this));
       return (
-        <div>
+        <div className="table-responsive">
           <table className="table table-condensed table-hover">
             <thead>
               <tr>
@@ -173,11 +173,13 @@ function(React, $, _,
   var BookTitle = React.createClass({
     getInitialState: function() {
       return {
-        hasBook: null
+        hasBook: null,
+        comments : new CommentListModel()
       };
     },
     componentDidMount: function() {
       this.refleshHasState();  
+      this.refleshComment();  
     },
     toHasState: function(){
       var bookOwnerList = new BookOwnerListModel();
@@ -216,6 +218,17 @@ function(React, $, _,
            swal("書籍の所有情報が取得できませんでいsた", "再度ブラウザをリロードしてください", "error");
         }.bind(this));
     },
+    refleshComment: function(){
+      this.state.comments.fetchComments(this.props.bookTitle, 
+        function(comments){
+          this.setState({
+            comments: comments
+          });
+        }.bind(this),
+        function(error){
+           swal("書籍のコメントが取得できませんでいsた", "再度ブラウザをリロードしてください", "error");
+        }.bind(this));
+    },
     handleDelete: function(event) {
       this.props.removeCallback(this.props.bookTitle);
     },
@@ -225,35 +238,196 @@ function(React, $, _,
     setISBN: function(event) {
       this.props.bookTitle.set("isbn", event.target.value);
     },
+    generateModalId: function(){
+      return "commentModal" + this.props.bookTitle.id;
+    },
     render: function() {
       var have_state_el = <input type="button" className="btn btn-success" value="自分持ってます" onClick={this.toHasState}/>;
       if (this.state.hasBook) {
         have_state_el = <input type="button" className="btn btn-danger" value="捨てた" onClick={this.toNotHaveState}/>;
       };
 
+      // 一意のIDを生成する
+      var dataTarget = "#" + this.generateModalId();
+      var dataId     = this.generateModalId();
+
+      var commentButtonTitle = this.state.comments.length + " コメント";
+
       return (
         <tr>
-          <td>
+          <td className="col-md-2">
             <div>
               {have_state_el}
             </div>
           </td>
-          <td>
+          <td className="col-md-6">
             <div>
               <input type="text" defaultValue={this.props.bookTitle.get("name")} className="form-control" ref="name" onChange={this.setName}/>
             </div>
           </td>
-          <td>
+          <td className="col-md-2">
             <div>
               <input type="text" defaultValue={this.props.bookTitle.get("isbn")} className="form-control" ref="isbn" onChange={this.setISBN}/>
             </div>
           </td>
-          <td>
+          <td className="col-md-1">
+            <div>
+              <div className="btn-group">
+                <button className="btn btn-info" data-toggle="modal" data-target={dataTarget}><span>{commentButtonTitle}</span></button>
+              </div>
+              <div className="modal fade" id={dataId} role="dialog" aria-hidden="true">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <button type="button" className="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span className="sr-only">Close</span></button>
+                      <h4 className="modal-title">コメント</h4>
+                    </div>
+                    <div className="modal-body">
+                      <CommentArea parent={this.props.bookTitle} comments={this.state.comments} refleshCallback={this.refleshComment} />
+                    </div>
+                    <div className="modal-footer">
+                      <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </td>
+          <td className="col-md-1">
             <div>
               <input type="button" className="btn btn-danger" value="削除" onClick={this.handleDelete}/>
             </div>
           </td>
         </tr>
+      );
+    }
+  });
+
+  /* コメント一覧 */
+  var CommentArea = React.createClass({
+    render: function() {
+
+      var comments_el = this.props.comments.map(function(comment, i) {
+        return <Comment comment={comment} key={comment.id}　refleshCallback={this.props.refleshCallback} />
+      }.bind(this));
+
+      return (
+        <div className="list-group">
+          {comments_el}
+          <CommentForm parent={this.props.parent} comments={this.props.comments}　refleshCallback={this.props.refleshCallback}/>
+        </div>
+      );
+    }
+  });
+
+  /* コメント一覧 */
+  var Comment = React.createClass({
+    getInitialState: function() {
+      return {
+        author: null,
+      };
+    },
+    componentDidMount: function() {
+      this.props.comment.get("author").fetch({
+        success: function(user){
+          this.setState({
+            author: user
+          });
+        }.bind(this),
+        error : function(data, error){
+          swal("コメント登録者の取得に失敗しました", "再度実行してください", "error");
+        }.bind(this)
+      });
+    },
+    removeComment: function(){
+
+      swal({
+        title: "コメントを削除しますか?",
+        text: "",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonClass: "btn-danger",
+        confirmButtonText: "削除とする",
+        closeOnConfirm: false
+      },
+      function(isConfirm){
+
+        this.props.comment.destroy({
+          success: function(data){
+            swal("コメントを削除しました", "", "success");
+            this.props.refleshCallback();
+          }.bind(this),
+          error: function(data, error){
+            swal("コメントの削除に失敗しました", "再度実行してください", "error");
+          }.bind(this)
+        });
+
+      }.bind(this));
+
+    },
+    render: function() {
+      return (
+        <a className="list-group-item">
+          <blockquote>
+            <p>{this.props.comment.get("text")}</p>
+            {this.state.author ? <footer>{this.state.author.get("screenname")}</footer> : null}
+            {this.state.author && 
+             this.state.author.get("username") === Parse.User.current().get("username") ? 
+                <button type="button" className="close" onClick={this.removeComment}>
+                  <span aria-hidden="true">&times;</span>
+                  <span className="sr-only">Close</span>
+                </button> : null}
+          </blockquote>
+        </a>
+      );
+    }
+  });
+
+  /* コメント登録 */
+  var CommentForm = React.createClass({
+    addComment: function(){
+      var text = this.refs.text.getDOMNode().value.trim();
+      if (!text) {
+        swal({
+          title: "エラー",
+          text: "未設定の項目が存在します",
+          type: "warning",
+          showCancelButton: false,
+          confirmButtonClass: "btn-danger",
+          confirmButtonText: "OK",
+          closeOnConfirm: false
+        });
+        return;
+      }
+
+      this.props.comments.create({
+        parent: this.props.parent,
+        text: text,
+        author: Parse.User.current()
+      }, {
+        success: function(data){
+          swal("コメントを投稿しました", "", "success");
+          this.props.refleshCallback();
+        }.bind(this),
+        error: function(data, error){
+          swal("コメントの登録に失敗しました", "再度実行してください", "error");
+        }.bind(this)
+
+      });
+
+    },
+    render: function() {
+      return (
+        <a className="list-group-item">
+          <div className="row">
+            <div className="col-md-10">
+              <input type="text" className="form-control" ref="text"/>
+            </div>
+            <div className="col-md-2">
+              <input type="button" className="btn btn-info" value="追加" onClick={this.addComment}/>
+            </div>
+          </div>
+        </a>
       );
     }
   });
---
+++ public/js/models/comment.js
@@ -0,0 +1,9 @@
+define(['parse'],function(Parse){
+
+    var Comment = Parse.Object.extend("Comment", {
+    });
+
+	return Comment;
+});
+
+
